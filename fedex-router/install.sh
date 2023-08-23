8c686f6ccffc1318c6a88c43ca0156c9962dff4f
vic1707 <28602203+vic1707@users.noreply.github.com>
2023-08-23 14:14:33 +0200

    feat: fedex configured from variables

--- fedex-router/install.sh
+++ fedex-router/install.sh
@@ -8,6 +8,14 @@
 # 1 GB HDD     #
 ################
 
+############ Variables ############
+ROUTER_PRIVATE_MASK=255.255.255.240
+ROUTER_PRIVATE_IP=10.0.0.1
+DHCP_LEASE_TIME=12h
+DHCP_LEASE_START=10.0.0.2
+DHCP_LEASE_END=10.0.0.14
+DNS_SERVERS=1.1.1.1,8.8.8.8
+
 # Install packages
 echo "Updating repositories and installing packages..."
 apk update
@@ -25,8 +33,8 @@ iface eth0 inet dhcp
 
 auto eth1
 iface eth1 inet static
-  address 10.10.1.1
-  netmask 255.255.255.0
+  address $ROUTER_PRIVATE_IP
+  netmask $ROUTER_PRIVATE_MASK
 EOF
 
 # Enable IP forwarding
@@ -50,7 +58,7 @@ rc-update add local default
 echo "Configuring iptables..."
 iptables -F
 iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-iptables -A FORWARD -i eth1 -j ACCEPT
+iptables -A FORWARD -i eth1 -o eth0 -j ACCEPT
 iptables -A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
 # Save iptables rules
 /etc/init.d/iptables save
@@ -61,9 +69,9 @@ rc-service iptables start
 echo "Configuring DHCP..."
 cat << EOF > /etc/dnsmasq.conf
 interface=eth1
-dhcp-range=10.10.1.10,10.10.1.20,255.255.255.0,24h
-dhcp-option=option:router,10.10.1.1
-dhcp-option=option:dns-server,1.1.1.1, 8.8.4.4
+dhcp-range=$DHCP_LEASE_START,$DHCP_LEASE_END,$ROUTER_PRIVATE_MASK,$DHCP_LEASE_TIME
+dhcp-option=option:router,$ROUTER_PRIVATE_IP
+dhcp-option=option:dns-server,$DNS_SERVERS
 EOF
 rc-update add dnsmasq
 rc-service dnsmasq start
