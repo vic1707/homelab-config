3cf9014122701d845a8fc76ee2940f4daaf7de1c
vic1707 <28602203+vic1707@users.noreply.github.com>
2023-09-05 15:35:21 +0200

    transmission better settings

--- marina/prod/dockers/transmission/pod.sh
+++ marina/prod/dockers/transmission/pod.sh
@@ -54,21 +54,25 @@ source_env() {
 }
 
 start() {
-  # TODO: check if `--device /dev/net/tun` and `CREATE_TUN_DEVICE=false` are needed
   podman run \
     --detach \
     --network shared \
     --name "$NAME" \
-    --cap-add=NET_ADMIN \
     --volume "/mnt/bhulk/$NAME/":/data \
     --volume "/mnt/config/$NAME/":/config \
+    --sysctl net.ipv6.conf.all.disable_ipv6=0 \
     --env TZ="Europe/Paris" \
+    --env ENABLE_UFW=true \
     --env WEBPROXY_ENABLED=false \
     --env TRANSMISSION_WEB_UI=flood-for-transmission \
+    `# allows restart on VPN failure` \
+    --env OPENVPN_OPTS="--inactive 3600 --ping 10 --ping-exit 60" \
     --env OPENVPN_PROVIDER="$OVPN_PROVIDER" \
     --env OPENVPN_CONFIG="$OVPN_CONFIG" \
     --env OPENVPN_USERNAME="$OVPN_USR" \
     --env OPENVPN_PASSWORD="$OVPN_PWD" \
+    `# wish i could disable these` \
+    --privileged \
     --env CREATE_TUN_DEVICE=false \
     --device /dev/net/tun \
     docker.io/haugene/transmission-openvpn:$VERSION
