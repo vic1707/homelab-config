1bec8b902ac56f57a605b5f416aa36711539bd80
vic1707 <28602203+vic1707@users.noreply.github.com>
2023-10-08 18:49:59 +0200

    increase values for jellyfin

--- marina/prod/dockers/jellyfin/pod.sh
+++ marina/prod/dockers/jellyfin/pod.sh
@@ -27,6 +27,8 @@ start() {
     --name "$NAME" \
     --publish 8096:8096/tcp \
     --env TZ="Europe/Paris" \
+    --env JELLYFIN_FFmpeg__probesize=5000000000 \
+    --env JELLYFIN_FFmpeg__analyzeduration=5000000000 \
     --volume "/mnt/config/$NAME":/config \
     --volume "/media/$NAME":/media:ro \
     --device nvidia.com/gpu=all \
