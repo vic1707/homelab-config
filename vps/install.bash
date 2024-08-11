be2665604443c20532075610f6a6beaabdd4bd82
vic1707 <28602203+vic1707@users.noreply.github.com>
2024-08-11 17:39:23 +0200

    add crowdsec to vps

--- vps/install.bash
+++ vps/install.bash
@@ -47,20 +47,18 @@ create_sudo_user() {
 
 ################################ Update System ################################
 echo "--- Updating repositories and installing packages... ---"
+curl -s https://install.crowdsec.net | sh
 dnf copr enable -y atim/bottom
 dnf install -y epel-release
-dnf install -y podman firewalld rsync
-dnf install -y bottom
+dnf install -y podman firewalld rsync crowdsec crowdsec-firewall-bouncer-iptables bottom
 dnf upgrade -y
-############################### Additional Users #############################
+############################## Additional Users ###############################
 echo "--- Creating additional users... ---"
 create_sudo_user "vic1707"
 create_user "bistro-tech"
 ################################## SSH Setup ##################################
 echo "--- Hardening SSH... ---"
 RDM_SSH_PORT=$(shuf -n 1 -i 10000-65500)
-echo "New SSH port: $RDM_SSH_PORT"
-
 sed -i "s/PermitRootLogin yes/PermitRootLogin no/g" /etc/ssh/sshd_config
 sed -i "s/#PermitRootLogin prohibit-password/PermitRootLogin no/g" /etc/ssh/sshd_config
 sed -i "s/#Port 22/Port $RDM_SSH_PORT/g" /etc/ssh/sshd_config
@@ -74,14 +72,20 @@ sed -i "s/#MaxStartups 10:30:100/MaxStartups 3:50:3/g" /etc/ssh/sshd_config
 sed -i "s/#TCPKeepAlive yes/TCPKeepAlive yes/g" /etc/ssh/sshd_config
 sed -i "s/#LoginGraceTime 2m/LoginGraceTime 0/g" /etc/ssh/sshd_config
 systemctl restart sshd
-################################# Firewall Setup ##############################
+############################### Firewall Setup ################################
 echo "--- Configuring firewall... ---"
 systemctl enable firewalld
 systemctl start firewalld
 firewall-cmd --permanent --remove-service=ssh
 firewall-cmd --permanent --add-port="$RDM_SSH_PORT/tcp"
 systemctl restart firewalld
-############################# Additional Settings ############################
+############################### Crowdsec Setup ################################
+echo "--- Configuring Crowdsec... ---"
+BOUNCER_API_KEY=$(cscli bouncers add firewall -o raw)
+sed -i "s/api_key: <API_KEY>/api_key: $BOUNCER_API_KEY/g" /etc/crowdsec/bouncers/crowdsec-firewall-bouncer.yaml
+systemctl enable crowdsec.service
+systemctl start crowdsec.service
+############################# Additional Settings #############################
 echo "--- Configuring additional settings... ---"
 ## Ask for password when invoking sudo
 echo "--- Configuring sudo... ---"
@@ -90,4 +94,9 @@ sed -i 's/NOPASSWD: //g' /etc/sudoers
 ## Disable IPv6
 echo "net.ipv6.conf.all.disable_ipv6=1" >> /etc/sysctl.conf
 
-echo "Don't forget to go read $PASSWORD_FILE"
+echo "
+    New SSH port: $RDM_SSH_PORT
+    Don't forget to:
+    - read $PASSWORD_FILE.
+    - enroll crowdsec: 'cscli console enroll -e context <key>'    
+"
