6956e1cbac0440b3861afaa2a9666b892146d1c0
vic1707 <28602203+vic1707@users.noreply.github.com>
2024-10-04 16:38:22 +0200

    redo pve setup

--- hydra/install.sh
+++ hydra/install.sh
@@ -39,8 +39,8 @@ echo "- Updating and upgrading"
 apt update && apt upgrade -y && apt dist-upgrade -y && pveam update
 
 ## Install git and fail2ban ##
-echo "- Installing git and fail2ban"
-apt install git fail2ban -y
+echo "- Installing git, nut and fail2ban"
+apt install git apcupsd fail2ban -y
 
 ## Configure fail2ban ##
 cat << EOF > /etc/fail2ban/filter.d/proxmox-virtual-environement.conf
@@ -61,29 +61,26 @@ ignoreregex =
 EOF
 
 # Enable & Restart Fail2Ban Service
-echo "- Restarting fail2ban service"
-systemctl enable fail2ban.service
-systemctl restart fail2ban.service
+echo "- Enabling fail2ban service"
+systemctl enable --now fail2ban.service
 
-## Deny root SSH and various hardening ##
-RDM_SSH_PORT=$(shuf -n 1 -i 10000-65500)
-sed -i "s/PermitRootLogin yes/PermitRootLogin no/g" /etc/ssh/sshd_config
-sed -i "s/#PermitRootLogin prohibit-password/PermitRootLogin no/g" /etc/ssh/sshd_config
-sed -i "s/#Port 22/Port $RDM_SSH_PORT/g" /etc/ssh/sshd_config
-sed -i "s/#ListenAddress 0.0.0.0/ListenAddress 0.0.0.0/g" /etc/ssh/sshd_config
-sed -i "s/#AddressFamily any/AddressFamily inet/g" /etc/ssh/sshd_config
-sed -i "s/#PermitEmptyPasswords no/PermitEmptyPasswords no/g" /etc/ssh/sshd_config
-sed -i "s/#LogLevel INFO/LogLevel VERBOSE/g" /etc/ssh/sshd_config
-sed -i "s/#MaxAuthTries 6/MaxAuthTries 3/g" /etc/ssh/sshd_config
-sed -i "s/#MaxSessions 10/MaxSessions 2/g" /etc/ssh/sshd_config
-sed -i "s/#PasswordAuthentication yes/PasswordAuthentication no/g" /etc/ssh/sshd_config
-sed -i "s/#MaxStartups 10:30:100/MaxStartups 3:50:3/g" /etc/ssh/sshd_config
-sed -i "s/#TCPKeepAlive yes/TCPKeepAlive yes/g" /etc/ssh/sshd_config
-sed -i "s/#LoginGraceTime 2m/LoginGraceTime 0/g" /etc/ssh/sshd_config
+# Disable SSH Service
+echo "- Disable SSH service"
+systemctl disable ssh sshd
 
-# Restart SSH Service
-echo "- Restarting SSH service"
-systemctl restart ssh sshd
+# Configure UPS
+echo "- Configure UPS"
+cat <<EOF > /etc/apcupsd/apcupsd.conf
+## apcupsd.conf v1.1 ##
+UPSTYPE usb
+UPSCABLE usb
+DEVICE 
+EOF
+sed -i "s/^ISCONFIGURED=no/ISCONFIGURED=yes/" /etc/default/apcupsd
+systemctl enable --now apcupsd
+
+echo "- 'apcaccess status'"
+apcaccess status
 
 ######### Add backups dir to ZFS pool #########
 ## Assuming that the pool is already created ##
