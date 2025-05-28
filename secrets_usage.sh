79130de966255dafb8ba2e497091716d7e6fbe75
vic1707 <28602203+vic1707@users.noreply.github.com>
2025-05-28 20:57:46 +0200

    simpler

--- secrets_usage.sh
+++ secrets_usage.sh
@@ -2,30 +2,29 @@
 
 set -euo pipefail
 
-declare -A MATCHES
+unused_secrets=()
 for secret in $(gopass ls -f); do
     output=$(rg --no-heading --line-number --fixed-strings "$secret" . || true)
-    MATCHES["$secret"]="$output"
-done
 
-unused_secrets=()
-
-for secret in "${!MATCHES[@]}"; do
-    if [[ -z ${MATCHES[$secret]} ]]; then
+    if [[ -z ${output} ]]; then
         unused_secrets+=("$secret")
         break
     fi
+
+    match_count=$(wc -l <<< "$output")
     echo -e "\033[1;34m🔐 Secret:\033[0m $secret"
+    echo -e "\033[1;36m   ↳ Found $match_count time(s)\033[0m"
+
     while IFS=: read -r file line _; do
         echo -e "\033[1;32m→ $file:$line\033[0m"
         bat --style=numbers --color=always --highlight-line "$line" --line-range "$line:$line" "$file"
-    done <<< "${MATCHES[$secret]}"
+    done <<< "$output"
     echo
 done
 
 if ! ((${#unused_secrets[@]} > 0)); then exit 0; fi
+
 echo -e "\033[1;33m⚠️ Unused secrets:\033[0m"
 for secret in "${unused_secrets[@]}"; do
     echo -e "  - $secret"
 done
-
