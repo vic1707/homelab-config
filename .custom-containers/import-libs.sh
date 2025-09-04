8910ac09c3cfa644d3e2ea8456d5184d56ea8ee8
vic1707 <28602203+vic1707@users.noreply.github.com>
2025-09-04 23:49:50 +0200

    lint should check hidden dirs

--- .custom-containers/import-libs.sh
+++ .custom-containers/import-libs.sh
@@ -13,17 +13,17 @@ ldd "$BIN_PATH" \
     | awk '{gsub(".*/","",$1); print $1}' \
     | while read -r lib; do
         # Skip virtual libs (like linux-vdso.so.1)
-        if [[ "$lib" =~ ^linux-.*\.so.*$ ]]; then
+        if [[ $lib =~ ^linux-.*\.so.*$ ]]; then
             echo "Skipping virtual lib: $lib"
             continue
         fi
 
         LIB_PATH=$(find /lib /usr/lib -name "$lib" -print -quit)
-        
-        if [[ -z "$LIB_PATH" ]]; then
+
+        if [[ -z $LIB_PATH ]]; then
             echo "Error: Could not find library '$lib'" >&2
             exit 1
         fi
-        
+
         cp -L "$LIB_PATH" "$TARGET_DIR"
     done
